14e07ea388e6a0a3d8b98391bfa0b9cf219540ad
Habibe Yılmaz <91463520+Habibeyilmazz@users.noreply.github.com>
2025-09-22 23:20:09 +0300

    nullpool

--- app/db.py
+++ app/db.py
@@ -1,9 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
+from sqlalchemy.pool import NullPool
 
 DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:postgres@localhost:5432/mini_crm")
 
-engine = create_engine(DATABASE_URL, echo=True, future=True)
+engine = create_engine(DATABASE_URL,poolclass=NullPool, echo=False, future=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
